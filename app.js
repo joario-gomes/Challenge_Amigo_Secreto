33e41e832c4e4d2c4c850015468d0dc293dbe8e5
Joario Gomes <joario.gomes@outlook.com>
2025-01-30 20:17:54 -0300

    Adição do teste de vazio na Função adicionarAmigo

--- app.js
+++ app.js
@@ -9,14 +9,24 @@ function sortearAmigo(){
 
 function adicionarAmigo(){
     let nome = document.querySelector('input').value.trim();//trim() remove os espaços apos o nome
+    
+    //Testando se o campo esta vazio
+    if(nome ===''){
+        alert('Por favor, insira um nome valido.');
+        return;
+    }
+
     let amigo = nome.toUpperCase(); //convertendo o nome para maiusculo
+
     //Testando se o nome já esta na lista
     if(amigoSecreto.includes(amigo)){
         alert(`O nome ${amigo} já esta na lista! Ensira outro nome.`);
     }else{
         amigoSecreto.push(amigo);
     }
+    
     limparCampo();
+    atualizarLista();
 }
 
 function limparCampo(){
@@ -33,4 +43,8 @@ function exibirTextoNaTela(tag, texto){
 function converteMaiuscula(nome) {
     let nomeMaiusculo = nome.toUpperCase();
     return nomeMaiusculo;
+}
+
+function atualizarLista(){
+
 }
