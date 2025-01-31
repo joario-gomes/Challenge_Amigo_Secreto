ceb5284bec394c323415472ae5c4459586eb971f
Joario Windstar <joario.gomes@outlook.com>
2025-01-31 17:24:36 -0300

    Limpeza do código

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigoSecreto = [];
 let listaAmigoSorteado = [];
-//let numeroLimite = amigoSecreto.length-1;
 
 console.log(sortearNumero());
 
 function sortearAmigo(){
     let numeroLimite = amigoSecreto.length-1;
     let numeroSorteado = parseInt(Math.random() * (numeroLimite + 1));
-    //let numeroSorteado = sortearNumero();
     if(listaAmigoSorteado.length == (amigoSecreto.length)){
         alert('Todos os amigos foram sorteados');
         amigoSecreto = [];
@@ -18,7 +16,6 @@ function sortearAmigo(){
         if(listaAmigoSorteado.includes(amigoSecreto[numeroSorteado])){
             sortearAmigo();
         }else{
-            //numeroSorteado = sortearNumero();
             let nome = amigoSecreto[numeroSorteado];
             listaAmigoSorteado.push(nome); //Falta colocar os já sorteados para que não seja sorteados novamente.
             let sorteado = document.getElementById('resultado');
@@ -72,21 +69,3 @@ function ocultarElemento(tag){
     let campo = document.getElementById(tag);
     campo.style.display = "none";
 }
-
-function desocultarElemento(tag){
-    let campo = document.getElementById(tag);
-    campo.style.display = "block";
-}
-
-function sortearNumero(){
-    let numeroSorteado = parseInt(Math.random() * (numeroLimite + 1));
-    return numeroSorteado;
-}
-
-/*
-//Apagar essa função
-function converteMaiuscula(nome) {
-    let nomeMaiusculo = nome.toUpperCase();
-    return nomeMaiusculo;
-}
-*/
